b0dedf1268e52c0e20a814c1a9f80a39fc525b5b
Lavisha Bhagnani <lavishabcr@gmail.com>
2025-11-16 08:28:32 +0000

    fix for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+module.exports = {
+  // IMPORTANT: use your repo name here
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/After-School-Lessons/"
+      : "/",
+};
+
+
