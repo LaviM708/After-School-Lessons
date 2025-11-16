6804bb790cfc383a64ea873aabda0ba3f6d4f408
Lavisha Bhagnani <lavishabcr@gmail.com>
2025-11-16 08:34:18 +0000

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 // vue.config.js
 module.exports = {
-  // IMPORTANT: use your repo name here
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "/After-School-Lessons/"
+      ? "./"
       : "/",
 };
+
 
 
