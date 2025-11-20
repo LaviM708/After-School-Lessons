99e393de7e75ebbb00a3d8d97bd16bff82798437
Lavisha Bhagnani <lavishabcr@gmail.com>
2025-11-20 08:48:43 +0000

    fixed bugs in cartCount

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -46,7 +46,7 @@
             class="badge rounded-pill bg-danger"
             style="position: absolute; top: 2px; right: 4px; font-size: 0.85rem; min-width: 18px;"
         >
-            {{ cart.length }}
+            {{ cartCount }}
         </span>
       </button>
     </div>
@@ -57,5 +57,16 @@
 export default {
   name: 'Navbar',
   props: ['pages', 'activePage', 'navLinkClick', 'cart'],
+
+  computed: {
+    cartCount() {
+      if (!Array.isArray(this.cart)) return 0;
+
+      // sum all quantities in the cart
+      return this.cart.reduce((sum, item) => {
+        return sum + (item.quantity || 0);
+      }, 0);
+    },
+  },
 };
 </script>
